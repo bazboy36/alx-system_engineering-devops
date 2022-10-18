bf571a66710dc4410dea40c01e231f855335e4b3
bazboy36 <shokunbiabdulbasit1@gmail.com>
2022-10-18 16:57:42 -0700

    WEBSERVER

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,23 @@
-# Puppet manifest to install nginx
-package { 'nginx':
-  ensure => installed,
+# Install and configure Nginx
+package {'nginx':
+  ensure   => 'installed',
+  provider => 'apt',
 }
 
-file_line { 'aaaaa':
+file {'Configure the home page':
+  ensure    => 'present',
+  path      => '/var/www/html/index.nginx-debian.html',
+  content   => 'Hello World\n'
+}
+
+file_line {'Redirection':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
-file { '/var/www/html/index.html':
-  content => 'Holberton School',
+  after  => 'server_name _;',
+  line   => 'location /redirect_me { rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent; }',
 }
 
 service { 'nginx':
-  ensure  => running,
+  ensure  => 'running',
   require => Package['nginx'],
 }
